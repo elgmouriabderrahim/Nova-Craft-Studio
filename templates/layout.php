5111031c5b7b8f6f386bcb48ae4f2250ecfec792
elgmouri abderrahim <ea.officialbox@gmail.com>
2025-12-02 11:36:51 +0000

    add layout page content

--- templates/layout.php
+++ templates/layout.php
@@ -3,9 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link href="/build/output.css" rel="stylesheet">
+    <title><?php echo $title?? "NovaCraft Studio - Accueil";?></title>
 </head>
 <body>
-    
+    <?php require_once  $_SERVER['DOCUMENT_ROOT'].'/templates/header.php';?>
+    <main>
+        <?php include $thispage;?>
+    </main>
+    <?php require_once  $_SERVER['DOCUMENT_ROOT'].'/templates/footer.php';?>
 </body>
 </html>
