1af750a120bd873b7cb3efbb74e85c5aa35672ff
elgmouri abderrahim <ea.officialbox@gmail.com>
2025-12-02 11:37:48 +0000

    fixing header links

--- templates/header.php
+++ templates/header.php
@@ -10,10 +10,10 @@
     <nav class="container mx-auto flex justify-between items-center py-4">
       <h1 class="text-2xl font-bold text-blue-600">DigitalWave</h1>
       <ul class="flex space-x-6">
-        <li><a href="/index.php" class="hover:text-blue-600">Accueil</a></li>
-        <li><a href="/views/services.php" class="hover:text-blue-600">Services</a></li>
-        <li><a href="/views/about.php" class="hover:text-blue-600">À propos</a></li>
-        <li><a href="/views/contact.php" class="text-blue-600 font-medium">Contact</a></li>
+        <li><a href="index.php?page=home" class="hover:text-blue-600">Accueil</a></li>
+        <li><a href="index.php?page=services" class="hover:text-blue-600">Services</a></li>
+        <li><a href="index.php?page=about" class="hover:text-blue-600">À propos</a></li>
+        <li><a href="index.php?page=contact" class="text-blue-600 font-medium">Contact</a></li>
       </ul>
     </nav>
   </header>
