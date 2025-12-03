ba00cb1353cd7e9dad8ae3e7b20939e1a05bf727
elgmouri abderrahim <ea.officialbox@gmail.com>
2025-12-03 10:57:23 +0000

    fix template footer page

--- templates/footer.php
+++ templates/footer.php
@@ -1,13 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <footer class="bg-gray-900 text-white py-6 text-center">
-        <p>© 2025 DigitalWave Solutions. Tous droits réservés.</p>
-    </footer>
-</body>
-</html>
+<footer class="bg-gray-900 text-white py-6 text-center">
+  <p>© 2025 DigitalWave Solutions. Tous droits réservés.</p>
+</footer>
