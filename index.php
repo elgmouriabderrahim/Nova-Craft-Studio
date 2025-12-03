00ada675803efe7c794c005082252d822503d50c
elgmouri abderrahim <ea.officialbox@gmail.com>
2025-12-03 10:49:27 +0000

    fixing routes logic in index

--- index.php
+++ index.php
@@ -1,42 +1,19 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-  <meta charset="UTF-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-</head>
-<body class="bg-gray-50 text-gray-800">
-  <?php 
-  $page = $_GET['page'] ?? 'home';
-  switch ($page) {
-      case 'home':
-          $title = "welcome to home";
-          $thispage = 'views/home.php';
-          break;
+<?php
 
-      case 'services':
-          $title = "NovaCraft Studio - Services";
-          $thispage = 'views/services.php';
-          break;
+$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
-      case 'contact':
-          $title = "NovaCraft Studio - contact";
-          $thispage = 'views/contact.php';
-          break;
 
-      case 'about':
-          $title = "NovaCraft Studio - about us";
-          $thispage = 'views/about.php';
-          break;
+$routes = [
+    '/'          => 'controllers/home.php',
+    '/services'  => 'controllers/services.php',
+    '/about'     => 'controllers/about.php',
+    '/contact'   => 'controllers/contact.php',
+];
 
-      default:
-          $title = "Not Found";
-          $thispage = 'views/404.php';
-  }
-  if($thispage == 'views/404.php')
-    include "views/404.php";
-  else
-  include "templates/layout.php";
-  ?>
-
-</body>
-</html>
+if (array_key_exists($uri, $routes)) {
+    require $routes[$uri];
+} else {
+    http_response_code(404);
+    require 'controllers/404.php';
+    exit;
+}
