1ebae87b43a5429c15d7ac57faa66d87c8f6eaca
elgmouri abderrahim <ea.officialbox@gmail.com>
2025-12-04 16:02:22 +0000

    fix contact logic place

--- views/contact.php
+++ views/contact.php
@@ -1,34 +1,15 @@
 <section class="container mx-auto py-16">
     <?php
-    $name = $email = $message = "";
-    $succes = false;
-    $name_regex = "/^[a-zA-Z\s]{3,}$/";
-    function test($data){
-        $data = htmlspecialchars($data);
-        $data = stripslashes($data);
-        $data = trim($data);
-        return $data;
-    }
-
-    if (!empty($_POST)) {
-        $name = test($_POST["name"]);
-        $email = test($_POST["email"]);
-        $message = test($_POST["message"]);
-    }
+    if (preg_match($name_regex, $name) && filter_var($email, FILTER_VALIDATE_EMAIL) && strlen($message) >= 5):
+        echo "<div class='max-w-xl mx-auto bg-green-100 text-green-700 p-4 rounded mb-4'>
+        Merci $name! Votre message a ete envoye avec succes.
+        </div>";
+        $name = $email = $message = "";
+        $succes = true;
+    endif;
     ?>
-
-    <?php if (preg_match($name_regex, $name) && filter_var($email, FILTER_VALIDATE_EMAIL) && strlen($message) >= 5): ?>
-        <div class="max-w-xl mx-auto bg-green-100 text-green-700 p-4 rounded mb-4">
-            <?php echo "Merci $name! Votre message a ete envoye avec succes."; ?>
-            <?php
-            $name = $email = $message = "";
-            $succes = true;
-            ?>
-
-        </div>
-    <?php endif; ?>
+    
     <h2 class="text-3xl font-bold mb-6 text-center">Contactez-nous</h2>
-
     <form class="max-w-xl mx-auto bg-white p-8 shadow-md rounded-lg space-y-4" method="POST">
         <input type="text" name="name" placeholder="Votre nom" class="w-full border px-4 py-2 rounded-lg" value="<?php echo $name; ?>">
         <span class= 'text-red-500'>*</span>
