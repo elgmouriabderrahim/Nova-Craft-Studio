5d6eb0e27906b2578d620b287ed2cab7c403aea2
elgmouri abderrahim <ea.officialbox@gmail.com>
2025-12-16 08:22:54 +0000

    fix index file

--- index.php
+++ index.php
@@ -1,19 +1,3 @@
 <?php
 
-$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-
-
-$routes = [
-    '/'          => 'controllers/home.php',
-    '/services'  => 'controllers/services.php',
-    '/about'     => 'controllers/about.php',
-    '/contact'   => 'controllers/contact.php',
-];
-
-if (array_key_exists($uri, $routes)) {
-    require $routes[$uri];
-} else {
-    http_response_code(404);
-    require 'controllers/404.php';
-    exit;
-}
+require_once $_SERVER['CODUMENT_ROOT'] . "routes/route.php";
